038070699032d244779ed19af172b12de6c6f5fc
Sandra <a21sande@student.his.se>
2024-12-11 11:40:03 +0100

    Changed r file to R project

---
+++ Seminair/R/Seminair_code.R
@@ -0,0 +1,10 @@
+#Load libraries
+library(edgeR)
+#library(clusterProfiler)
+library(enrichplot)
+library(org.Hs.eg.db)
+library(openxlsx)
+
+#documenting
+library(devtools)
+document()
--- Seminair_code.R
+++
@@ -1,6 +0,0 @@
-#Load libraries
-library(edgeR)
-library(clusterProfiler)
-library(enrichplot)
-library(org.Hs.eg.db)
-library(openxlsx)
