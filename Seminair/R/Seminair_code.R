5e4c8637ed254e51351808a6305a1d5da4a1d76e
Sandra <a21sande@student.his.se>
2024-12-16 11:41:14 +0100

    Added statistical analyses of data

--- Seminair/R/Seminair_code.R
+++ Seminair/R/Seminair_code.R
@@ -9,6 +9,8 @@ library(DESeq2)
 #Before running the code, change working directory to the directory of the
 #files being imported
 
+#PART 1
+
 #Import RNA count and sample table
 counts <- read.table(file = "E-MTAB-2523.counts.txt",
                           header = TRUE,
@@ -25,6 +27,29 @@ samples <- read.table(file = "E-MTAB-2523_sample table.txt",
 #Data filtering, based on mean log2 cmp
 meanLog2CPM <- rowMeans(log2(cpm(counts) + 1))
 counts <- counts[meanLog2CPM >1, ]
+
+#PART 2
+
+#Extract data from sample table, this data is later used for design of dds
+#object
+individual <- samples$individual
+disease <- samples$disease
+
+#Create dds object
+dds <- DESeqDataSetFromMatrix(
+  countData = counts,
+  design = ~ individual + disease,
+  colData = data.frame(
+    individual = individual,
+    disease = disease))
+
+dds <- DESeq(dds)
+res <- results(dds)
+
+#FDR and log2 fold change
+Filtered <- res[which(res$padj < 0.05 & abs(res$log2FoldChange) > 1), ]
+
+
 
 
 
