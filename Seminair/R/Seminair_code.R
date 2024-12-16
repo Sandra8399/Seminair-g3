59ddb2c974d21a85dd5d5ae58848eb7cdf8b77bc
Sandra <a21sande@student.his.se>
2024-12-16 10:36:36 +0100

    Imported and filteres RNA-seq data in R file

--- Seminair/R/Seminair_code.R
+++ Seminair/R/Seminair_code.R
@@ -4,7 +4,27 @@ library(edgeR)
 library(enrichplot)
 library(org.Hs.eg.db)
 library(openxlsx)
+library(DESeq2)
 
-#documenting
-library(devtools)
-document()
+#Before running the code, change working directory to the directory of the
+#files being imported
+
+#Import RNA count and sample table
+counts <- read.table(file = "E-MTAB-2523.counts.txt",
+                          header = TRUE,
+                          as.is = TRUE,
+                          sep = "\t",
+                          row.names = 1)
+
+samples <- read.table(file = "E-MTAB-2523_sample table.txt",
+                          header = TRUE,
+                          as.is = TRUE,
+                          sep = "\t",
+                          row.names = 1)
+
+#Data filtering, based on mean log2 cmp
+meanLog2CPM <- rowMeans(log2(cpm(counts) + 1))
+counts <- counts[meanLog2CPM >1, ]
+
+
+
